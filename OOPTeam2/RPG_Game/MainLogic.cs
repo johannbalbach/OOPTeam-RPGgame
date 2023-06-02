a99226c967117e45877cadff131e1a0d238ea202
Liid <51267703+L11D@users.noreply.github.com>
2023-06-02 16:53:49 +0700

    init

---
+++ OOPTeam2/RPG_Game/MainLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOPTeam2.RPG_Game
+{
+    internal class MainLogic
+    {
+        public MainLogic() 
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
